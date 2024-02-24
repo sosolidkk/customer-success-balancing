bbfbed46aeac7c70bf5106c60cb017517cc1e21e
sosolidkk <pedrochaveslimas3@gmail.com>
2024-02-23 22:19:29 -0300

    First version with all specs expect the third

--- customer_success_balancing.rb
+++ customer_success_balancing.rb
@@ -1,7 +1,14 @@
 require 'minitest/autorun'
 require 'timeout'
 
+# Class responsible for returning the CS id that serves the most customers
+# @param customer_success [Array<Integer>] customer success experience level
+# @param customers [Array<Integer>] customers experience level
+# @param away_customer_success [Array<Integer>] unavailable customer success ids
+# @return [Integer] customer_success id
 class CustomerSuccessBalancing
+  attr_reader :customer_success, :customers, :away_customer_success
+
   def initialize(customer_success, customers, away_customer_success)
     @customer_success = customer_success
     @customers = customers
@@ -10,10 +17,41 @@ class CustomerSuccessBalancing
 
   # Returns the ID of the customer success with most customers
   def execute
-    # Write your solution here
+    # Remove all away CS's
+    available_customer_success = customer_success.reject { |item| away_customer_success.include?(item[:id]) }
+
+    # Sort all CS's by score
+    available_customer_success.sort_by! { |item| item[:score] }
+
+    # Sort all CS's by score
+    customers.sort_by! { |item| item[:score] }
+
+    matched_customers = {}
+
+    available_customer_success.each do |available_cs|
+      score = available_cs[:score]
+
+      users_count = (customers.dup - customers.delete_if { |customer| customer[:score] <= score }).count
+
+      matched_customers[available_cs[:id]] = users_count
+    end
+
+    id, max_value = matched_customers.max_by { |_, value| value }
+    count_of_max_value = matched_customers.count { |_, value| value == max_value }
+
+    count_of_max_value > 1 ? 0 : id
+  end
+
+  private
+
+  def maximum_away_customer_success_size = (customer_success.size / 2).floor
+
+  def valid_away_customer_success_size?
+    maximum_away_customer_success_size <= away_customer_success.size || away_customer_success.empty?
   end
 end
 
+# Class responsible for executing the test suite
 class CustomerSuccessBalancingTests < Minitest::Test
   def test_scenario_one
     balancer = CustomerSuccessBalancing.new(
@@ -36,7 +74,7 @@ class CustomerSuccessBalancingTests < Minitest::Test
   def test_scenario_three
     balancer = CustomerSuccessBalancing.new(
       build_scores(Array(1..999)),
-      build_scores(Array.new(10000, 998)),
+      build_scores(Array.new(10_000, 998)),
       [999]
     )
     result = Timeout.timeout(1.0) { balancer.execute }
@@ -92,7 +130,7 @@ class CustomerSuccessBalancingTests < Minitest::Test
 
   def build_scores(scores)
     scores.map.with_index do |score, index|
-      { id: index + 1, score: score }
+      { id: index + 1, score: }
     end
   end
 end
